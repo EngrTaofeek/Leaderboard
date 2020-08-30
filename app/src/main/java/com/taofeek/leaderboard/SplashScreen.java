639c3ba3d45e9804e092b743911a1da631e352b8
EngrTaofeek <oduola.taofeekkola@gmail.com>
2020-08-30 12:35:42 +0100

    Implemented splash screen

---
+++ app/src/main/java/com/taofeek/leaderboard/SplashScreen.java
@@ -0,0 +1,40 @@
+package com.taofeek.leaderboard;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+
+public class SplashScreen extends AppCompatActivity {
+    private ImageView imageView;
+    private static int splashTimeOut = 5000;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().hide();
+        }
+
+        imageView = findViewById(R.id.splash_image);
+        // splash screen to open the Main Activity
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent= new Intent(SplashScreen.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, splashTimeOut);
+        // create the animation
+        Animation myAnim = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.splashanimation);
+        imageView.startAnimation(myAnim);
+    
+    }
+}
