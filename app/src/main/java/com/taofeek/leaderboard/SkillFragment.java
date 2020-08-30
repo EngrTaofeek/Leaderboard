6e5d04ce0af8f26d97d46dce99b5b14c54eb8e10
EngrTaofeek <oduola.taofeekkola@gmail.com>
2020-08-30 07:42:58 +0100

    Initial commit

---
+++ app/src/main/java/com/taofeek/leaderboard/SkillFragment.java
@@ -0,0 +1,21 @@
+package com.taofeek.leaderboard;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class SkillFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_skill, container, false);
+    }
+}
