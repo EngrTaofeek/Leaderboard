5ac682fd8fdffbaf077bcc59d8ba88a2de17c1be
EngrTaofeek <oduola.taofeekkola@gmail.com>
2020-08-31 12:28:08 +0100

    confirmation dialog with errors

--- app/src/main/java/com/taofeek/leaderboard/MainActivity.java
+++ app/src/main/java/com/taofeek/leaderboard/MainActivity.java
@@ -1,5 +1,6 @@
 package com.taofeek.leaderboard;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
@@ -12,6 +13,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
 
 import com.taofeek.leaderboard.ui.main.SectionsPagerAdapter;
 
@@ -26,6 +28,15 @@ public class MainActivity extends AppCompatActivity {
         viewPager.setAdapter(sectionsPagerAdapter);
         TabLayout tabs = findViewById(R.id.tabs);
         tabs.setupWithViewPager(viewPager);
+
+        Button submit_button = findViewById(R.id.submit_button);
+        submit_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this,SubmitActivity.class);
+                startActivity(intent);
+            }
+        });
 
     }
 }
--- app/src/main/java/com/taofeek/leaderboard/SubmitActivity.java
+++ app/src/main/java/com/taofeek/leaderboard/SubmitActivity.java
@@ -4,10 +4,16 @@ import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.app.Dialog;
 import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class SubmitActivity extends AppCompatActivity {
     private Toolbar toolbar;
+    Button submit;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +23,26 @@ public class SubmitActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
         ActionBar actionbar = getSupportActionBar();
         actionbar.setDisplayHomeAsUpEnabled(true);
+        submit = findViewById(R.id.submit_button_act);
+        submit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showConfirmationDialog();
+            }
+        });
 
+    }
+
+    public void showConfirmationDialog() {
+        Dialog dialog = new Dialog(SubmitActivity.this);
+        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        dialog.setContentView(R.layout.confirmation_dialog);
+        Button button_yes = dialog.findViewById(R.id.button_yes);
+        button_yes.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(SubmitActivity.this, "It is working guys",Toast.LENGTH_LONG);
+            }
+        });
     }
 }
